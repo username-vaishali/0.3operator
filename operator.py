67a7430e5755697543da10f273cf206c800155ec
unknown <palvaishali19@gmail.com>
2023-12-28 19:40:32 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,31 @@
+a = 80
+b = 70
+
+# Arithmatic Operators
+print("The value of a+b is ", 80+70)
+print("The value of a-b is ", 80-70)
+print("The value of a*b is ", 80*70)
+print("The value of a/b is ", 80/70)
+
+# Assignment Operators
+a = 34
+a -= 12
+a *= 12
+a /= 12
+print(a)
+
+# Comparison Operators
+b = (14<7)
+b = (14>7)
+b = (14==7)
+b = (14!=7)
+print(b)
+
+# Logical Operators
+bool1 = True
+bool2 = False
+print("The value of bool1 and bool2 is",(bool1 and bool2))
+print("The value of bool1 or bool2 is",(bool1 or bool2))
+print("The value of bool1 not bool2 is",(not bool2))
+
+
